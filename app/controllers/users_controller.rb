2f6a6f90a693df61f6e585f7dfd8fa6fe53235d7
VENTILYATOR228 <pobeditel.com@gmail.com>
2021-12-22 18:43:17 +0300

    Add crud opperations to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @campaigns = @user.campaigns
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :campaigns, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
 end
